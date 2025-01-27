40a6ace9fe47fa2f9ebbc5d17c8a09e9f056b43a
Guzman Zugnoni <gooseiman@protonmail.com>
2025-01-26 22:01:03 -0300

    fix deprecated configs

--- configuration.nix
+++ configuration.nix
@@ -48,22 +48,24 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
