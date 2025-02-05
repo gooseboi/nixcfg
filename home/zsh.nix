e6f2f91eaabf1fb4ffe3404edc6481ec0213dd8b
Guzman Zugnoni <gooseiman@protonmail.com>
2025-02-05 19:36:47 -0300

    add zsh colors

--- home/zsh.nix
+++ home/zsh.nix
@@ -44,6 +44,8 @@ in {
     };
 
     initExtra = ''
+      autoload -U colors && colors
+
       PS1="%{$fg[blue]%}[%D{%f/%m/%y} %D{%H:%M:%S}] %B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
     '';
   };
