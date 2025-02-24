3dc6d1bcf1239bbd767d266a234c930c2f85a1be
Guzman Zugnoni <gooseiman@protonmail.com>
2025-02-24 02:28:43 -0300

    move hyprland to another folder

--- home/default.nix
+++ home/default.nix
@@ -4,13 +4,13 @@
       ./alacritty.nix
       ./eza.nix
       ./git.nix
-      ./hyprland.nix
       ./jujutsu.nix
       ./tmux.nix
       ./zathura.nix
       ./zsh.nix
     ]
     ++ [
+      ./hyprland
       ./scripts
     ];
 }
