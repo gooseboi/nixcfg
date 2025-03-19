5ca692b94282ecd7a0a0a5d03fdb5d5de8319265
Guzman Zugnoni <gooseiman@protonmail.com>
2025-03-19 02:23:01 -0300

    fix nvim config

--- home/nvim/easymotion.nix
+++ home/nvim/easymotion.nix
@@ -11,7 +11,7 @@
         local kset = vim.keymap.set
         kset("n", "<C-f>", "<Plug>(easymotion-overwin-w)", { noremap = true, silent = true })
         kset("", "<leader>unused", "<Plug>(easymotion-overwin-w)")
-        kset("", "<C-f>", "<Plug>(easymotion-overwin-w)", { noremap = true, silent = true })
+        kset("", "<leader><leader>", "<C-^>") -- Easymotion resets this
       end
     },
   '';
