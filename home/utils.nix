53e5dad13634baccd984ee75463b7a69b89cf8a0
Guzman Zugnoni <gooseiman@protonmail.com>
2025-03-20 18:46:07 -0300

    add btop config

--- home/utils.nix
+++ home/utils.nix
@@ -19,54 +19,58 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    home = {
-      packages = with pkgs; [
-        btop
-        btrfs-progs
-        curl
-        dig
-        dosfstools
-        dust
-        exfatprogs
-        fd
-        ffmpeg-full
-        file
-        fzf
-        gdu
-        htop
-        hyperfine
-        killall
-        libqalculate
-        magic-wormhole
-        man
-        man-db
-        man-pages
-        man-pages-posix
-        moreutils
-        nh
-        ntfs3g
-        oha
-        p7zip
-        ripgrep
-        socat
-        tealdeer
-        tree
-        unzip
-        watch
-        wget
-      ];
+    home.packages = with pkgs; [
+      btrfs-progs
+      curl
+      dig
+      dosfstools
+      dust
+      exfatprogs
+      fd
+      ffmpeg-full
+      file
+      fzf
+      gdu
+      htop
+      hyperfine
+      killall
+      libqalculate
+      magic-wormhole
+      man
+      man-db
+      man-pages
+      man-pages-posix
+      moreutils
+      nh
+      ntfs3g
+      oha
+      p7zip
+      ripgrep
+      socat
+      tealdeer
+      tree
+      unzip
+      watch
+      wget
+    ];
 
-      sessionVariables = {
-        RIPGREP_CONFIG_PATH = ripgrep_config;
-        WGETRC = wgetrc;
+    programs.btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
       };
+    };
 
-      file = {
-        "${myLib.removeHomeDirPrefixStr ripgrep_config}".text = "";
-        "${myLib.removeHomeDirPrefixStr wgetrc}".text = ''
-          hsts-file = ${config.xdg.cacheHome}/wget-hsts
-        '';
-      };
+    home.sessionVariables = {
+      RIPGREP_CONFIG_PATH = ripgrep_config;
+      WGETRC = wgetrc;
+    };
+
+    home.file = {
+      "${myLib.removeHomeDirPrefixStr ripgrep_config}".text = "";
+      "${myLib.removeHomeDirPrefixStr wgetrc}".text = ''
+        hsts-file = ${config.xdg.cacheHome}/wget-hsts
+      '';
     };
   };
 }
