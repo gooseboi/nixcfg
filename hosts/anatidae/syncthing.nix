2fb395f7bc7e5989906330f52db7e7cb46dd286e
Guzman Zugnoni <gooseiman@protonmail.com>
2025-03-21 14:41:32 -0300

    fix syncthing config

---
+++ hosts/anatidae/syncthing.nix
@@ -0,0 +1,54 @@
+{config, ...}: {
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
+  services.syncthing = {
+    enable = true;
+
+    user = config.chonkos.user;
+    dataDir = "/home/${config.chonkos.user}";
+    overrideDevices = true;
+    overrideFolders = true;
+
+    settings = {
+      gui = {
+        theme = "dark";
+      };
+
+      devices = {
+        "anser" = {id = "3IBIEL6-6QNWOKJ-NZ5UJHB-QNC54FV-3I7EU7G-M5NPWDH-AWGHDCB-WFXV6AE";};
+        "cowboy" = {
+          id = "RD7VJNX-HFZE4BB-E45QICM-TPSRK6P-CRU3VRM-ANT7GUZ-LJZ3N3S-UZFIHA3";
+          addresses = ["tcp://gooseman.net"];
+        };
+      };
+
+      folders = {
+        "dox" = {
+          enable = true;
+          id = "4lkyi-u5rlc";
+          path = "~/dox";
+          devices = ["anser"];
+        };
+        "music" = {
+          enable = true;
+          id = "hygaw-rg6ui";
+          path = "~/music";
+          devices = ["anser"];
+        };
+        "pix" = {
+          enable = true;
+          id = "zdjfq-o6neh";
+          path = "~/pix";
+          devices = ["anser"];
+        };
+        "vids" = {
+          enable = true;
+          id = "kqsee-nrs9r";
+          path = "~/vids";
+          devices = ["anser"];
+        };
+      };
+
+      options.urAccepted = -1; # Disable telemetry.
+    };
+  };
+}
