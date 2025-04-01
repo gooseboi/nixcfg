aa7dc840b95e2e5ff99acc44abfeff48887d7e39
Guzman Zugnoni <gooseiman@protonmail.com>
2025-04-01 11:13:53 -0300

    try adding ferdium

--- hosts/swordsmachine/services/ferdium.nix
+++ hosts/swordsmachine/services/ferdium.nix
@@ -1,9 +1,56 @@
-{...}: {
+{config, ...}: let
+  serviceName = "ferdium-server";
+  user = serviceName;
+  group = serviceName;
+
+  stateDirName = serviceName;
+  stateDir = "/var/lib/${stateDirName}";
+  dataDir = "${stateDir}/data";
+  recipesDir = "${stateDir}/recipes";
+in {
+  assertions = [
+    {
+      assertion = config.virtualisation.oci-containers.backend == "podman";
+      message = ''
+        This doesn't work with docker.
+      '';
+    }
+  ];
+
+  users.users.${user} = {
+    group = group;
+    isSystemUser = true;
+  };
+  users.groups.${group} = {};
+
+  systemd.services."${config.virtualisation.oci-containers.backend}-${serviceName}".serviceConfig = {
+    StateDirectory = "${stateDirName}";
+  };
+
   virtualisation.oci-containers.containers = {
-    ferdium-server = {
-      image = "ferdium/ferdium-server";
+    "${serviceName}" = {
+      image = "docker.io/ferdium/ferdium-server";
       autoStart = true;
-      ports = ["127.0.0.1:1234:1234"];
+
+      user = "${user}:{group}";
+      podman.user = user;
+
+      volumes = [
+        "${dataDir}:/data"
+        "${recipesDir}:/app/recipes"
+      ];
+      ports = ["127.0.0.1:3333:3333"];
+      environment = {
+        NODE_ENV = "production";
+        APP_URL = "https://ferdium.gooseman.net";
+        DB_CONNECTION = "sqlite";
+        IS_CREATION_ENABLED = "false";
+        IS_DASHBOARD_ENABLED = "true";
+        IS_REGISTRATION_ENABLED = "false";
+        CONNECT_WITH_FRANZ = "false";
+        DATA_DIR = "/data";
+        JWT_USE_PEM = "true";
+      };
     };
   };
 }
