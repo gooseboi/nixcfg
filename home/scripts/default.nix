48519b63f75d1d08e546eea729a3ac95d15f90fc
Guzman Zugnoni <gooseiman@protonmail.com>
2025-04-18 16:55:56 -0300

    common: add yt-dlp scripts

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -34,5 +34,85 @@ in {
           wl-clipboard
           yt-dlp
         ]);
+
+      xdg.configFile."yt/yt-dl-channel.conf".text = ''
+        -i
+        -c
+        -o "%(uploader)s/%(playlist_title)s/%(upload_date)s - %(title)s - (%(duration)ss) [%(resolution)s] [%(id)s].%(ext)s"
+
+        # Cookies
+        --cookies ~/.local/share/youtube_cookies.txt
+
+        # Uniform Format
+        --prefer-ffmpeg
+        --merge-output-format mkv
+
+        # Get All Subs to SRT
+        --convert-subs srt
+        --all-subs
+        --embed-subs
+
+        # Get metadata
+        --add-metadata
+        --write-description
+        --embed-chapters
+        --write-thumbnail
+
+        # Debug
+        -v
+      '';
+
+      xdg.configFile."yt/yt-dl-playlist.conf".text = ''
+        -i
+        -c
+        -o "%(playlist_title)s/%(playlist_index)s - %(uploader)s - %(upload_date)s - %(title)s - (%(duration)ss) [%(resolution)s] [%(id)s].%(ext)s"
+
+        # Cookies
+        --cookies ~/.local/share/youtube_cookies.txt
+
+        # Uniform Format
+        --prefer-ffmpeg
+        --merge-output-format mkv
+
+        # Get All Subs to SRT
+        --convert-subs srt
+        --all-subs
+        --embed-subs
+
+        # Get metadata
+        --add-metadata
+        --write-description
+        --write-thumbnail
+        --embed-chapters
+
+        # Debug
+        -v
+      '';
+
+      xdg.configFile."yt/yt-dl-vid.conf".text = ''
+        -i
+        -c
+        -o "%(uploader)s - %(upload_date)s - %(title)s - (%(duration)ss) [%(resolution)s] [%(id)s].%(ext)s"
+
+        # Cookies
+        --cookies ~/.local/share/youtube_cookies.txt
+
+        # Uniform Format
+        --prefer-ffmpeg
+        --merge-output-format mkv
+
+        # Get All Subs to SRT
+        --convert-subs srt
+        --all-subs
+        --embed-subs
+
+        # Get metadata
+        --add-metadata
+        --embed-chapters
+        --embed-thumbnail
+
+        # Debug
+        -v
+      '';
     };
 }
