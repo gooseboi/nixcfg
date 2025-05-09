1baf6048cc89d1731ee169c3d9efc113ec49bbcf
Guzman Zugnoni <gooseiman@protonmail.com>
2025-05-09 14:33:36 -0300

    common(network-manager): add alias to display wifi password

--- modules/network-manager.nix
+++ modules/network-manager.nix
@@ -13,5 +13,8 @@ in {
     networking.networkmanager.enable = true;
 
     users.users.${cfg.user}.extraGroups = ["networkmanager"];
+
+    # Blatantly stolen from https://github.com/RGBCube/NCC/blob/06cce18e7259e060a65af2af2bf0e609cd8e9a2c/modules/linux/network-manager.nix
+    environment.shellAliases.wifi = "nmcli dev wifi show-password";
   };
 }
