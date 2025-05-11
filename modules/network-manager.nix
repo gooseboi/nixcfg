72c3341bf53aa227faea4b1e5bde74944c306fa1
Guzman Zugnoni <gooseiman@protonmail.com>
2025-05-11 17:58:37 -0300

    common(network-manager): move wait-online.enable = false to network-manager

--- modules/network-manager.nix
+++ modules/network-manager.nix
@@ -14,6 +14,8 @@ in {
 
     users.users.${cfg.user}.extraGroups = ["networkmanager"];
 
+    systemd.network.wait-online.enable = false;
+
     # Blatantly stolen from https://github.com/RGBCube/NCC/blob/06cce18e7259e060a65af2af2bf0e609cd8e9a2c/modules/linux/network-manager.nix
     environment.shellAliases.wifi = "nmcli dev wifi show-password";
   };
--- modules/networking.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  config = lib.mkIf config.networking.networkmanager.enable {
-    systemd.network.wait-online.enable = false;
-  };
-}
