9e10941b47ed084e23aa23b9d87c876e4a6abc91
Guzman Zugnoni <gooseiman@protonmail.com>
2025-05-20 17:27:17 -0300

    swordsmachine(caddy): hardening

--- hosts/swordsmachine/services/caddy.nix
+++ hosts/swordsmachine/services/caddy.nix
@@ -42,4 +42,19 @@ in {
           cfg
         ));
   };
+
+  # Hardening
+  systemd.services.caddy.serviceConfig = {
+    ProtectClock = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+    ProtectControlGroups = true;
+    RestrictNamespaces = true;
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+  };
 }
