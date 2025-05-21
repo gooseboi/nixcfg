545936162da8d704849f1e69cd28ea7edf510c99
Guzman Zugnoni <gooseiman@protonmail.com>
2025-05-21 12:06:19 -0300

    common(git): add ssh signing and use isDesktop

--- home/git.nix
+++ home/git.nix
@@ -1,62 +1,82 @@
 {
-  programs.git = {
-    enable = true;
-    userName = "Guzman Zugnoni";
-    userEmail = "gooseiman@protonmail.com";
+  systemConfig,
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkMerge mkIf;
+in {
+  programs.git = mkMerge [
+    {
+      enable = true;
+      userName = "Guzman Zugnoni";
+      userEmail = "gooseiman@protonmail.com";
 
-    extraConfig = {
-      color.ui = true;
+      extraConfig = {
+        color.ui = true;
 
-      init.defaultBranch = "master";
+        init.defaultBranch = "master";
 
-      core = {
-        editor = "nvim";
-        autocrlf = "input";
-        safecrlf = true;
-      };
-
-      merge.tool = "nvim -d";
-    };
-
-    includes = let
-      uniConfig = {
-        contents = {
-          user = {
-            name = "Guzman Zugnoni";
-            email = "guzman.zugnoni@correo.ucu.edu.uy";
-            signingkey = "~/.ssh/id_uni.pub";
-          };
-
-          commit.gpgsign = true;
-          gpg.format = "ssh";
-          core.sshCommand = "ssh -i ~/.ssh/id_uni";
-
-          url."ssh://git@github.com/".insteadOf = "https://github.com";
+        core = {
+          editor = "nvim";
+          autocrlf = "input";
+          safecrlf = true;
         };
-      };
-    in [
-      (uniConfig
-        // {
-          condition = "gitdir:~/dev/uni/**";
-        })
-      (uniConfig
-        // {
-          condition = "gitdir:~/dox/uni/**";
-        })
-    ];
 
-    aliases = {
-      c = "commit";
-      s = "status";
-      d = "diff";
-      ap = "add -p";
-      ds = "diff --staged";
-      rp = "restore -p";
-      rs = "restore --staged";
-      rsp = "restore --staged -p";
-      lol = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      dw = "diff --word-diff";
-      dws = "diff --word-diff --staged";
-    };
-  };
+        merge.tool = "nvim -d";
+      };
+
+      aliases = {
+        c = "commit";
+        s = "status";
+        d = "diff";
+        ap = "add -p";
+        ds = "diff --staged";
+        rp = "restore -p";
+        rs = "restore --staged";
+        rsp = "restore --staged -p";
+        lol = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        dw = "diff --word-diff";
+        dws = "diff --word-diff --staged";
+      };
+    }
+
+    (mkIf systemConfig.chonkos.isDesktop {
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        signByDefault = true;
+      };
+
+      extraConfig = {
+        gpg.format = "ssh";
+      };
+
+      includes = let
+        uniConfig = {
+          contents = {
+            user = {
+              name = "Guzman Zugnoni";
+              email = "guzman.zugnoni@correo.ucu.edu.uy";
+              signingkey = "~/.ssh/id_uni.pub";
+            };
+
+            commit.gpgsign = true;
+            gpg.format = "ssh";
+            core.sshCommand = "ssh -i ~/.ssh/id_uni";
+
+            url."ssh://git@github.com/".insteadOf = "https://github.com";
+          };
+        };
+      in [
+        (uniConfig
+          // {
+            condition = "gitdir:~/dev/uni/**";
+          })
+        (uniConfig
+          // {
+            condition = "gitdir:~/dox/uni/**";
+          })
+      ];
+    })
+  ];
 }
