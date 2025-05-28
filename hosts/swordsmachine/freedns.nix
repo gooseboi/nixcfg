2b18439325941e883bfc749e95953fc83c0cedae
Guzman Zugnoni <gooseiman@protonmail.com>
2025-05-28 11:58:21 -0300

    swordsmachine(freedns): harden service

--- hosts/swordsmachine/freedns.nix
+++ hosts/swordsmachine/freedns.nix
@@ -31,6 +31,19 @@
     serviceConfig = {
       Type = "oneshot";
       User = "root";
+
+      # Hardening
+      ProtectClock = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectKernelLogs = true;
+      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+      ProtectControlGroups = true;
+      RestrictNamespaces = true;
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
     };
   };
 }
