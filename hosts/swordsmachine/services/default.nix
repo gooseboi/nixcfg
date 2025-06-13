dfa347d2c4e6338bbf24f526895834da075fbe75
Guzman Zugnoni <gooseiman@protonmail.com>
2025-06-13 03:18:20 -0300

    swordsmachine: remove todo

--- hosts/swordsmachine/services/default.nix
+++ hosts/swordsmachine/services/default.nix
@@ -9,7 +9,6 @@ in {
   # TODO: Radarr, Sonarr (https://wiki.servarr.com/radarr)
   # TODO: Syncserver (https://github.com/mozilla-services/syncstorage-rs)
   # TODO: Prometheus, Grafana (https://github.com/rgbcube/ncc/blob/5125a31e7f613943e83696814a223b7d2a1c5191/hosts/best/grafana/default.nix)
-  # TODO: Anubis (https://anubis.techaro.lol)
   # TODO: Paperless-ngx (https://docs.paperless-ngx.com)
 
   imports = [
