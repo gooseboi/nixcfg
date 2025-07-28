afd2d843b6ae62d79c40d9e14fdcedd1bef75a27
Guzman Zugnoni <gooseiman@protonmail.com>
2025-07-28 01:31:16 -0300

    common(networkmanager): ignore interfaces created by other software

--- modules/network-manager.nix
+++ modules/network-manager.nix
@@ -16,6 +16,25 @@ in {
 
     users.users.${config.chonkos.user}.extraGroups = ["networkmanager"];
 
+    networking.networkmanager.unmanaged = [
+      "type:bridge"
+
+      # Tailscale
+      "interface-name:tailscale*"
+      # Tailscale
+      "interface-name:ts*"
+
+      # Docker bridge
+      "interface-name:docker0"
+      # Docker container interfaces
+      "interface-name:veth*"
+      # Docker container interfaces
+      "interface-name:br*"
+
+      # QEMU
+      "interface-name:virbr*"
+    ];
+
     systemd.network.wait-online.enable = false;
 
     # Blatantly stolen from https://github.com/RGBCube/NCC/blob/06cce18e7259e060a65af2af2bf0e609cd8e9a2c/modules/linux/network-manager.nix
