1978342580059320651aa77aaae49ab70022f681
Guzman Zugnoni <gooseiman@protonmail.com>
2025-08-01 18:49:17 -0300

    common: add syncthing module

--- hosts/anatidae/syncthing.nix
+++ hosts/anatidae/syncthing.nix
@@ -1,23 +1,4 @@
 {config, ...}: {
-  systemd.services.syncthing = {
-    environment.STNODEFAULTFOLDER = "true";
-
-    # Hardening
-    serviceConfig = {
-      ProtectClock = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectKernelLogs = true;
-      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
-      ProtectControlGroups = true;
-      RestrictNamespaces = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-    };
-  };
-
   services.syncthing = {
     enable = true;
 
