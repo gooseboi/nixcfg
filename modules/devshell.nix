52cb3005bacb382630bbb2e32370e18dc67b8259
Guzman Zugnoni <gooseiman@protonmail.com>
2025-08-09 23:33:59 -0300

    common(devshell): add devsh_run and refactor code

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: let
-  inherit (lib) filter mkEnableOption mkIf mkOption types;
+  inherit (lib) filter flatten mkEnableOption mkIf mkOption optional types;
   inherit (lib.strings) concatStringsSep;
 
   cfg = config.chonkos.devshell;
@@ -17,129 +17,110 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    # The idea from this was mainly from: https://www.reddit.com/r/NixOS/comments/p17r5f/comment/h8jrmns/
-    environment = {
-      systemPackages = [
-        (
-          pkgs.writeShellScriptBin "activate_devshell.sh" (
-            let
-              makeSearchPath = packages: subDir: sep:
-                packages
-                |> filter (p: p != null)
-                |> map (p: "${p}/${subDir}")
-                |> filter builtins.pathExists
-                |> concatStringsSep sep;
+  config = let
+    makeSearchPath = packages: subDir: sep:
+      packages
+      |> filter (p: p != null)
+      |> map (p: "${p}/${subDir}")
+      |> filter builtins.pathExists
+      |> concatStringsSep sep;
 
-              libraryPath = makeSearchPath cfg.packages "lib" ":";
+    withDevAndOut = packages:
+      packages
+      |> map (p: [p.out] ++ (optional (p ? dev) p.dev))
+      |> flatten;
 
-              cPath = makeSearchPath cfg.packages "include" ":";
+    packages = withDevAndOut cfg.packages;
 
-              pkgConfigPath = makeSearchPath cfg.packages "lib/pkgconfig" ":";
-              pkgConfigPath' = makeSearchPath cfg.packages "share/pkgconfig" ":";
-            in
-              /*
-              bash
-              */
-              ''
-                export CPATH="$CPATH:${cPath}";
-                export LIBRARY_PATH="$LIBRARY_PATH:${libraryPath}";
-                export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libraryPath}";
-                export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${pkgConfigPath}:${pkgConfigPath'}";
-              ''
-          )
-        )
-        pkgs.pkg-config
-      ];
+    libraryPath = makeSearchPath packages "lib" ":";
 
-      shellAliases = {
-        activate_devshell = ". activate_devshell.sh";
+    cPath = makeSearchPath packages "include" ":";
+
+    pkgConfigPath = makeSearchPath packages "lib/pkgconfig" ":";
+    pkgConfigPath' = makeSearchPath packages "share/pkgconfig" ":";
+
+    activationText =
+      /*
+      bash
+      */
+      ''
+        export CPATH="$CPATH:${cPath}";
+        export LIBRARY_PATH="$LIBRARY_PATH:${libraryPath}";
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libraryPath}";
+        export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${pkgConfigPath}:${pkgConfigPath'}";
+      '';
+  in
+    mkIf cfg.enable {
+      # The idea from this was mainly from: https://www.reddit.com/r/NixOS/comments/p17r5f/comment/h8jrmns/
+      environment = {
+        systemPackages = [
+          (pkgs.writeShellScriptBin "devsh_activate"
+            /*
+            bash
+            */
+            ''
+              ${activationText}
+              $SHELL
+            '')
+          (pkgs.writeShellScriptBin "devsh_run"
+            /*
+            bash
+            */
+            ''
+              ${activationText}
+              $@
+            '')
+          pkgs.pkg-config
+        ];
       };
-    };
 
-    chonkos.devshell.packages = with pkgs; [
-      atk
-      atk.dev
-      cairo
-      cairo.dev
-      dbus
-      dbus.dev
-      ffmpeg-full.dev
-      ffmpeg-full.out
-      gcc
-      gdk-pixbuf
-      gdk-pixbuf.dev
-      glfw2
-      glfw3
-      glib.dev
-      glib.out
-      graphene.dev
-      graphene.out
-      gtk3
-      gtk3.dev
-      gtk3.dev
-      gtk3.out
-      gtk4-layer-shell.dev
-      gtk4-layer-shell.out
-      gtk4.dev
-      gtk4.out
-      harfbuzz
-      harfbuzz.dev
-      libadwaita.dev
-      libadwaita.out
-      libcap
-      libcap.dev
-      libdrm.dev
-      libdrm.out
-      libpulseaudio
-      libpulseaudio.dev
-      libsoup_3
-      libsoup_3.dev
-      libva
-      libva.dev
-      libxkbcommon
-      libxkbcommon.dev
-      libxml2
-      libxml2.dev
-      libxslt.dev
-      libz
-      libz.dev
-      pango.dev
-      pango.out
-      pipewire.dev
-      pipewire.out
-      raylib
-      sdl3
-      sdl3.dev
-      stdenv.cc.cc.lib
-      vulkan-headers
-      vulkan-loader.dev
-      vulkan-loader.out
-      wayland-scanner.dev
-      wayland-scanner.out
-      wayland.dev
-      wayland.out
-      webkitgtk_4_1
-      webkitgtk_4_1.dev
-      webkitgtk_6_0
-      webkitgtk_6_0.dev
-      xorg.libX11.dev
-      xorg.libX11.out
-      xorg.libXcomposite.dev
-      xorg.libXcomposite.out
-      xorg.libXdamage.dev
-      xorg.libXdamage.out
-      xorg.libXfixes.dev
-      xorg.libXfixes.out
-      xorg.libXi.dev
-      xorg.libXi.out
-      xorg.libXrandr.dev
-      xorg.libXrandr.out
-      xorg.libXrender.dev
-      xorg.libXrender.out
-      xorg.xorgproto
-      zlib
-      zlib.dev
-    ];
-  };
+      chonkos.devshell.packages = with pkgs; [
+        atk
+        cairo
+        dbus
+        ffmpeg-full
+        gcc
+        gdk-pixbuf
+        glfw2
+        glfw3
+        glib
+        graphene
+        gtk3
+        gtk3
+        gtk4
+        gtk4-layer-shell
+        harfbuzz
+        libadwaita
+        libcap
+        libdrm
+        libglvnd
+        libpulseaudio
+        libsoup_3
+        libva
+        libxkbcommon
+        libxml2
+        libxslt
+        libz
+        pango
+        pipewire
+        raylib
+        sdl3
+        stdenv.cc.cc.lib
+        vulkan-headers
+        vulkan-loader
+        wayland
+        wayland-scanner
+        webkitgtk_4_1
+        webkitgtk_6_0
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.xorgproto
+        zlib
+      ];
+    };
 }
