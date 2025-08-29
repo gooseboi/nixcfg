50b7b5265bf188112733ceb6755e0b0299ff783d
Guzman Zugnoni <gooseiman@protonmail.com>
2025-08-28 22:41:45 -0300

    swordsmachine(prometheus): fmt

--- hosts/swordsmachine/services/prometheus.nix
+++ hosts/swordsmachine/services/prometheus.nix
@@ -48,7 +48,7 @@ in {
       stateDir = lib.removePrefix "/var/lib/" cfg.dataDir;
 
       retentionTime = "5w";
-      globalConfig.scrape_interval= "10s";
+      globalConfig.scrape_interval = "10s";
 
       exporters = {
         node = {
