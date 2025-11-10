8b02a70852e276731deb9a81c4e63ab73325ff6b
Guzman Zugnoni <gooseiman@protonmail.com>
2025-11-10 00:34:34 -0300

    common: move caddy hardening to own module

--- hosts/swordsmachine/services/caddy.nix
+++ hosts/swordsmachine/services/caddy.nix
@@ -42,28 +42,12 @@ in {
     useAnubis = mkConst true;
   };
 
-  # TODO: Add metrics (https://caddyserver.com/docs/metrics)
   # TODO: X-Real-IP
 
   config = mkMerge [
     {
       services.caddy = {
         inherit (cfg) enable;
-      };
-
-      # Hardening
-      systemd.services.caddy.serviceConfig = {
-        ProtectClock = true;
-        ProtectKernelTunables = true;
-        ProtectKernelModules = true;
-        ProtectKernelLogs = true;
-        SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
-        ProtectControlGroups = true;
-        RestrictNamespaces = true;
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
       };
     }
     (mkIf (!cfg.useAnubis) {
---
+++ modules/services/caddy.nix
@@ -0,0 +1,19 @@
+{
+  # TODO: Add metrics (https://caddyserver.com/docs/metrics)
+  config = {
+      # Hardening
+      systemd.services.caddy.serviceConfig = {
+        ProtectClock = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true;
+        SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+        ProtectControlGroups = true;
+        RestrictNamespaces = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+      };
+  };
+}
