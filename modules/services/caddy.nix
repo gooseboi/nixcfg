ede2baafd139c860cd48cc447cd1c815ba88559a
Guzman Zugnoni <gooseiman@protonmail.com>
2025-11-10 01:07:02 -0300

    common(services): only harden caddy if enabled

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,19 +1,29 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    ;
+  cfg = config.services.caddy;
+in {
   # TODO: Add metrics (https://caddyserver.com/docs/metrics)
-  config = {
-      # Hardening
-      systemd.services.caddy.serviceConfig = {
-        ProtectClock = true;
-        ProtectKernelTunables = true;
-        ProtectKernelModules = true;
-        ProtectKernelLogs = true;
-        SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
-        ProtectControlGroups = true;
-        RestrictNamespaces = true;
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-      };
+  config = mkIf cfg.enable {
+    # Hardening
+    systemd.services.caddy.serviceConfig = {
+      ProtectClock = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectKernelLogs = true;
+      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+      ProtectControlGroups = true;
+      RestrictNamespaces = true;
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+    };
   };
 }
