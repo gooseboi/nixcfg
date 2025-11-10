7f06568f6e763ff6e2a891e4af08616fc89a491b
Guzman Zugnoni <gooseiman@protonmail.com>
2025-11-10 00:18:40 -0300

    flake: add cowboy key and rekey secrets

--- keys.nix
+++ keys.nix
@@ -1,7 +1,8 @@
 let
   keys = {
-    chonk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuUT9jJWba9PeWFpaEyypGMSk1F4hO5rYwtiruh9+uZ";
     anatidae = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILg5PTKfTTXffGubprKXFg98mKhnpPTkdAXmcBNon/UU";
+    chonk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuUT9jJWba9PeWFpaEyypGMSk1F4hO5rYwtiruh9+uZ";
+    cowboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHm3UweR+NM95TBsYQ237BX9y1jvT6wSGgnUjaFkewlP";
     printer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbJtMv8Tr6XODoIfoTq89ytU+9DTmU1X1h+T7vU0Fiy";
     swordsmachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYB1t65P81JR8kY60KKiKqwtGzvhWbjIk9uDzMETlue";
   };
