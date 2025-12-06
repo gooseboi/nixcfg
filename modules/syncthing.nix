10da4315533a14e691e468ce792ec6eecff18361
Guzman Zugnoni <gooseiman@protonmail.com>
2025-12-06 14:10:04 -0300

    common: dont add syncthing config if not enabled

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,35 +1,48 @@
-{config, ...}: {
-  systemd.services.syncthing = {
-    environment.STNODEFAULTFOLDER = "true";
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    ;
+in {
+  config = mkIf config.services.syncthing.enable {
+    systemd.services = {
+      syncthing = {
+        environment.STNODEFAULTFOLDER = "true";
 
-    # Hardening
-    serviceConfig = {
-      ProtectClock = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectKernelLogs = true;
-      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
-      ProtectControlGroups = true;
-      RestrictNamespaces = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-    };
-  };
-
-  services.syncthing = {
-    user = config.chonkos.user;
-
-    overrideDevices = true;
-    overrideFolders = true;
-
-    settings = {
-      gui = {
-        theme = "dark";
+        # Hardening
+        serviceConfig = {
+          ProtectClock = true;
+          ProtectKernelTunables = true;
+          ProtectKernelModules = true;
+          ProtectKernelLogs = true;
+          SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+          ProtectControlGroups = true;
+          RestrictNamespaces = true;
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+        };
       };
+    };
 
-      options.urAccepted = -1; # Disable telemetry.
+    services.syncthing = {
+      user = config.chonkos.user;
+
+      overrideDevices = true;
+      overrideFolders = true;
+
+      settings = {
+        gui = {
+          theme = "dark";
+        };
+
+        options.urAccepted = -1; # Disable telemetry.
+      };
     };
   };
 }
