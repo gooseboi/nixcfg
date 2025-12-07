8e35de16c4f0c9e5096966675a7548eabe3b9e39
Guzman Zugnoni <gooseiman@protonmail.com>
2025-12-07 12:15:32 -0300

    common: update rss-proxy

--- parts/pkgs/packages/rss-proxy.nix
+++ parts/pkgs/packages/rss-proxy.nix
@@ -10,8 +10,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "gooseboi";
     repo = "rss-proxy";
-    rev = "9609bb3112055dc7d75a470bd6910af1379c1521";
-    hash = "sha256-l5JCtBEJNgqBZdQyEp165KgHsJcFjnZiDy0ZJ0ACMxM=";
+    rev = "d3e6becd8681788c96a3f8943c722908d4356107";
+    hash = "sha256-b7SBjLWLtmCwmiYzKCY7+sVU/S0r56bZdb4j6vcu4MQ=";
   };
 
   cargoHash = "sha256-6rZhBsqjPnDq+hx0vdu0vcKWUXyQEAgyPwExrWrRNhE=";
