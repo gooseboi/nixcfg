7086dde9bdfc623154e7af9c45c76135caacce84
Guzman Zugnoni <gooseiman@protonmail.com>
2025-12-23 19:13:31 -0300

    swordsmachine(services): automatically import

--- hosts/swordsmachine/services/default.nix
+++ hosts/swordsmachine/services/default.nix
@@ -1,23 +1,16 @@
-{
+{lib, ...}: let
+  inherit
+    (lib)
+    listNix
+    remove
+    ;
+in {
   # TODO: Immich (https://immich.app/)
   # TODO: Radarr, Sonarr (https://wiki.servarr.com/radarr)
   # TODO: Murmur (https://github.com/mumble-voip/mumble)
   # TODO: Paperless-ngx (https://docs.paperless-ngx.com)
 
-  imports = [
-    ./anki-sync-server.nix
-    ./caddy.nix
-    ./ferdium.nix
-    ./forgejo.nix
-    ./grafana.nix
-    ./prometheus.nix
-    ./radicale.nix
-    ./readeck.nix
-    ./rss-proxy.nix
-    ./stirling-pdf.nix
-    ./suwayomi-server.nix
-    ./vaultwarden.nix
-  ];
+  imports = listNix ./. |> remove ./default.nix;
 
   chonkos.services.postgresql = {
     enable = true;
