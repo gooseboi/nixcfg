4fa0859b501bcab8e29011635633e814bd1bdf15
Guzman Zugnoni <gooseiman@protonmail.com>
2025-12-28 15:16:26 -0300

    common(nix-ld): only enable on desktops by default

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,8 +1,34 @@
-{pkgs, ...}: {
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      stdenv.cc.cc.lib
-    ];
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    mkOption
+    types
+    ;
+
+  inherit (config.chonkos) isDesktop;
+
+  cfg = config.chonkos.nix-ld;
+in {
+  options.chonkos.nix-ld = {
+    enable = mkOption {
+      description = "whether to enable nix-ld";
+      type = types.bool;
+      default = isDesktop;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc.lib
+      ];
+    };
   };
 }
