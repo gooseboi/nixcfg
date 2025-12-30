4eac10fa9313d17ddb8ebf66a2b6337a272d5faa
Guzman Zugnoni <gooseiman@protonmail.com>
2025-12-30 20:47:28 -0300

    common(caddy): fix systemd file created even if not enabled

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -7,8 +7,9 @@
     (lib)
     mkEnableOption
     mkIf
-    types
+    mkMerge
     mkOption
+    types
     ;
 in {
   options.chonkos.services.prometheus.exporters.caddy = {
@@ -25,31 +26,35 @@ in {
     };
   };
 
-  config = {
-    services.caddy = let
-      cfg = config.chonkos.services.prometheus.exporters.caddy;
-    in {
-      # TODO: Set port from options
-      globalConfig = mkIf cfg.enable ''
-        metrics {
-          per_host
-        }
-      '';
-    };
+  config = mkMerge [
+    {
+      services.caddy = let
+        cfg = config.chonkos.services.prometheus.exporters.caddy;
+      in {
+        # TODO: Set port from options
+        globalConfig = mkIf cfg.enable ''
+          metrics {
+            per_host
+          }
+        '';
+      };
+    }
 
-    # Hardening
-    systemd.services.caddy.serviceConfig = mkIf config.services.caddy.enable {
-      ProtectClock = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectKernelLogs = true;
-      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
-      ProtectControlGroups = true;
-      RestrictNamespaces = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-    };
-  };
+    (mkIf config.services.caddy.enable {
+      # Hardening
+      systemd.services.caddy.serviceConfig = {
+        ProtectClock = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true;
+        SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+        ProtectControlGroups = true;
+        RestrictNamespaces = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+      };
+    })
+  ];
 }
