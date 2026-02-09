5979f9847d21713d34dd3f0bc594581d180d7245
Guzman Zugnoni <gooseiman@protonmail.com>
2026-02-09 10:16:52 -0300

    indicus(syncthing): add sm-a107m device

--- hosts/indicus/syncthing.nix
+++ hosts/indicus/syncthing.nix
@@ -16,6 +16,7 @@
       devices = {
         "anser" = {id = "3IBIEL6-6QNWOKJ-NZ5UJHB-QNC54FV-3I7EU7G-M5NPWDH-AWGHDCB-WFXV6AE";};
         "pixel7" = {id = "4TQ7FPC-3CE33Y6-Q27XXAY-URCGU7S-A7AUGT4-LX77I3Z-AS5RGZ6-MWWSRAU";};
+        "SM-A107M" = {id = "WGBHLXR-C7UZMPY-DY3SGCU-LJECWCW-SDQZ55L-RM6AT3U-I4AEZ6C-IGC7NQ4";};
       };
 
       folders = {
@@ -29,7 +30,7 @@
           enable = true;
           id = "hygaw-rg6ui";
           path = "~/music";
-          devices = ["anser" "pixel7"];
+          devices = ["anser" "pixel7" "SM-A107M"];
         };
         "pix" = {
           enable = true;
