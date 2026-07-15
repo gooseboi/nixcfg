27910344b20f322f9892826b99b621becfcde87e
Guzman Zugnoni <gooseiman@protonmail.com>
2026-07-14 22:38:45 -0300

    pkgs: fix dcpt420w driver

--- parts/pkgs/packages/dcpt420w-driver.nix
+++ parts/pkgs/packages/dcpt420w-driver.nix
@@ -14,9 +14,21 @@
   gnugrep,
   which,
   gawk,
+  libredirect,
 }: let
   version = "3.5.0";
   model = "dcpt420w";
+
+  binPath = lib.makeBinPath [
+    gawk
+    ghostscript
+    a2ps
+    file
+    gnused
+    gnugrep
+    coreutils
+    which
+  ];
 in
   stdenv.mkDerivation {
     pname = "cups-brother-${model}";
@@ -50,31 +62,32 @@ in
 
       # NOTE: The interpreter directive of this file (/usr/bin/perl) is patched
       # by the fixup phase automagically, so we don't need to worry.
+      #
+      # The wrapper renames the file from filter_<model> to
+      # .filter_<model>-wrapped, but the regex doesn't handle this
       substituteInPlace $LPDDIR/filter_${model} \
-      --replace "/usr/bin/pdf2ps" "${ghostscript}/bin/pdf2ps"
+        --replace-fail "/usr/bin/pdf2ps" "${ghostscript}/bin/pdf2ps" \
+        --replace-fail '$PRINTER =~ s/\.pl$//;' '$PRINTER =~ s/\.pl$//; $PRINTER =~ s/-wrapped$//;'
+
+      # NOTE: The lpdwrapper Perl script extracts the model name from its own
+      # path using a regex that assumes /opt/ prefix. On NixOS the path starts
+      # with /nix/store/, so the regex must be relaxed to match any prefix.
+      substituteInPlace $WRAPPERDIR/brother_lpdwrapper_${model} \
+        --replace-fail 's/^\/opt\/.*\/Printers\///g' 's/^.*\/opt\/.*\/Printers\///g'
 
       wrapProgram $LPDDIR/filter_${model} \
-        --prefix PATH ":" ${
-        lib.makeBinPath [
-          gawk
-          ghostscript
-          a2ps
-          file
-          gnused
-          gnugrep
-          coreutils
-          which
-        ]
-      }
+        --prefix PATH ":" ${binPath}
 
       patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
       $LPDDIR/br${model}filter
       wrapProgram $LPDDIR/br${model}filter \
+        --set LD_PRELOAD "${libredirect}/lib/libredirect.so" \
         --set NIX_REDIRECTS /opt=$out/opt
 
       patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
       $LPDDIR/brprintconf_${model}
       wrapProgram $LPDDIR/brprintconf_${model} \
+        --set LD_PRELOAD "${libredirect}/lib/libredirect.so" \
         --set NIX_REDIRECTS /opt=$out/opt
 
       mkdir -p $out/lib/cups/filter/
@@ -82,7 +95,13 @@ in
       ln -s $WRAPPERDIR/brother_lpdwrapper_${model} $out/lib/cups/filter/
 
       mkdir -p $out/share/cups/model
-      ln -s $WRAPPERDIR/brother_${model}_printer_en.ppd $out/share/cups/model/
+      ln -s $WRAPPERDIR/brother_${model}_printer_en.ppd $out/share/cups/model
+
+      wrapProgram $WRAPPERDIR/brother_lpdwrapper_${model} \
+        --prefix PATH ":" ${binPath}
+
+      wrapProgram $WRAPPERDIR/cupswrapper${model} \
+        --prefix PATH ":" ${binPath}
     '';
 
     meta = {
