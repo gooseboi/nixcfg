c728e4a7c713acb215df08c7537bd378faae4738
Guzman Zugnoni <gooseiman@protonmail.com>
2026-09-22 14:08:00 -0300

    canagicus: add atuin sync server

---
+++ hosts/canagicus/services/atuin-sync.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    ;
+  inherit (config.networking) domain;
+
+  enable = true;
+
+  port = 8875;
+in {
+  config = mkIf enable {
+    services.atuin = {
+      enable = true;
+      inherit port;
+
+      database = {
+        uri = "postgres://atuin?host=/run/postgresql";
+        createLocally = false;
+      };
+
+
+      maxHistoryLength = 65536;
+    };
+
+    chonkos.services.postgresql.ensure = ["atuin"];
+
+    chonkos.services.reverse-proxy.hosts.atuin = {
+      target = "http://127.0.0.1:${toString port}";
+      targetType = "tcp";
+      domain = "atuin.${domain}";
+    };
+  };
+}
